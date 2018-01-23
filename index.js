cd7040ef9834376f44301a0e6071bf4061c55efb
Pierre Cauchois <pierreca@microsoft.com>
2018-01-23 00:22:03 -0800

    Add Device Methods samples - not fully working (uuid encoding issue)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var rhea = require('rhea');
 var uuid = require('uuid');
+var uuidBuffer = require('uuid-buffer');
 
 var hubName = '<hub name>';
 var hostName = hubName + '.azure-devices.net';
@@ -12,15 +13,60 @@ rhea.on('connection_open', (context) => {
     console.log('connection error');
   });
 
-  var receiver = context.connection.open_receiver( {
+  /**
+   * D2C
+   */
+  var msgqueue = [];
+  var d2c_sender = context.connection.open_sender( {
+    target: '/devices/' + deviceId + '/messages/events',
+    properties: {
+      'com.microsoft:client-version': 'rhea-test'
+    }
+  });
+  d2c_sender.on('sender_open', (context) => {
+    console.log('d2c sender opened');
+    msgqueue.push(d2c_sender.send({
+      message_id: uuid.v4(),
+      body: 'foo',
+      application_properties: {
+        apppropkey: 'apppropvalue'
+      },
+      message_annotations: {
+        status: 201
+      }
+    }));
+
+    d2c_sender.on('accepted', (context) => {
+      for (var i = 0; i < msgqueue.length; i++) {
+        if (msgqueue[i] === context.delivery) {
+          console.log('accepted');
+        } else {
+          console.log('nope');
+        }
+      }
+    })
+  });
+
+  d2c_sender.on('error', (context) => {
+    console.log('sender error');
+  });
+
+  d2c_sender.on('sender_close', (context) => {
+    console.log('sender closed');
+  });
+
+  /**
+   * C2D
+   */
+  var c2d_receiver = context.connection.open_receiver( {
     source: '/devices/' + deviceId + '/messages/devicebound',
     autoaccept: false // true by default
   });
-  receiver.on('receiver_open', (context) => {
-    console.log('receiver opened');
+  c2d_receiver.on('receiver_open', (context) => {
+    console.log('c2d receiver opened');
   });
 
-  receiver.on('message', (context) => {
+  c2d_receiver.on('message', (context) => {
     console.log('message received');
     console.log(context.message.body.content.toString());
     context.delivery.accept();
@@ -28,38 +74,69 @@ rhea.on('connection_open', (context) => {
     // context.delivery.release();
   });
 
-  // var msgqueue = [];
-  // var sender = context.connection.open_sender('/devices/' + deviceId + '/messages/events');
-  // sender.on('sender_open', (context) => {
-  //   console.log('sender opened');
-  //   msgqueue.push(sender.send({
-  //     id: uuid.v4(),
-  //     body: 'foo'
-  //   }));
-  //   msgqueue.push(sender.send({
-  //     id: uuid.v4(),
-  //     body: 'bar'
-  //   }));
+  /**
+   * Direct Methods
+   */
+  var method_response_queue = [];
+  var method_sender = context.connection.open_sender( {
+    target: '/devices/' + deviceId + '/methods/devicebound',
+    properties: {
+      'com.microsoft:client-version': 'rhea-test',
+      'com.microsoft:api-version': '2017-06-30',
+      'com.microsoft:channel-correlation-id': deviceId
+    }
+  });
+  method_sender.on('sender_open', (context) => {
+    console.log('method sender opened');
 
-  //   sender.on('accepted', (context) => {
-  //     for (var i = 0; i < msgqueue.length; i++) {
-  //       if (msgqueue[i] === context.delivery) {
-  //         console.log('accepted');
-  //       } else {
-  //         console.log('nope');
-  //       }
-  //     }
-  //   })
-  // });
+    method_sender.on('accepted', (context) => {
+      for (var i = 0; i < method_response_queue.length; i++) {
+        if (method_response_queue[i] === context.delivery) {
+          console.log('accepted');
+        } else {
+          console.log('nope');
+        }
+      }
+    })
+  });
 
-  // sender.on('error', (context) => {
-  //   console.log('sender error');
-  // });
+  method_sender.on('error', (context) => {
+    console.log('sender error');
+  });
 
-  // sender.on('sender_close', (context) => {
-  //   console.log('sender closed');
-  // });
+  method_sender.on('sender_close', (context) => {
+    console.log('sender closed');
+  });
 
+  var method_receiver = context.connection.open_receiver( {
+    source: '/devices/' + deviceId + '/methods/devicebound',
+    properties: {
+      'com.microsoft:client-version': 'rhea-test',
+      'com.microsoft:api-version': '2017-06-30',
+      'com.microsoft:channel-correlation-id': deviceId
+    }
+  });
+  method_receiver.on('receiver_open', (context) => {
+    console.log('method receiver opened');
+  });
+
+  method_receiver.on('message', (context) => {
+    console.log('method request received');
+    console.log('method name: ' + context.message.application_properties['IoThub-methodname']);
+    console.log('payload: ' + context.message.body.content.toString());
+    console.log('request id: ' + context.message.correlation_id);
+    console.log('uuidbuffer: ' + uuidBuffer.toString(context.message.correlation_id));
+    method_response_queue.push(method_sender.send({
+      // TODO: need to figure out if we can "force" the encoding of the correlation_id as a UUID. Right now it's encoded as a string and IoT Hub rejects it.
+      correlation_id: uuidBuffer.toString(context.message.correlation_id),
+      body: {
+        'foo': 'bar'
+      },
+      application_properties: {
+        'IoThub-status': 200
+      }
+    }));
+  });
 });
 
 rhea.on('error', (context) => {
